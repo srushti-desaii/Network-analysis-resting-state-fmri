8f3ee0902ff5c4341fec50e535bede5760400e3e
Srushti Desai <srushtidesai0611@gmail.com>
2026-01-26 09:51:06 -0500

    Add weighted lattice network generator for SWP computation.

--- R/swp_functions.R
+++ R/swp_functions.R
@@ -22,3 +22,35 @@ assign_weights = function(adj, epsilon = 1) {
   return(weights)
 }
 
+
+# Generate a Weighted Small-World
+# Network(Lattice network)
+generate_weighted_sw = function(N, r) {
+  
+  # Initialize adjacency matrix
+  adj_matrix = matrix(0, nrow = N, ncol = N)  
+  
+  # Assign positions to nodes
+  node_positions = 1:N  
+  
+  # Initialize a ring lattice
+  for (i in 1:(N - 1)) {
+    for (j in (i + 1):N) {
+      
+      # Compute the shortest distance
+      dist = min(abs(node_positions[i] - node_positions[j]),
+                 N - abs(node_positions[i] - node_positions[j]))
+      
+      # If the distance is less than or equal to r
+      if (dist <= r) {
+        adj_matrix[i, j] = 1  
+        adj_matrix[j, i] = 1 
+      }
+    }
+  }
+  
+  # Assign weights based on distance
+  weight_matrix = assign_weights(adj_matrix)
+  
+  return(weight_matrix)
+}
