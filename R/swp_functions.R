15da6c451173779a730bc87be1788a184acafa05
Srushti Desai <srushtidesai0611@gmail.com>
2026-01-27 11:39:01 -0500

    Add graph rewiring function for random network generation

--- R/swp_functions.R
+++ R/swp_functions.R
@@ -54,3 +54,40 @@ generate_weighted_sw = function(N, r) {
   
   return(weight_matrix)
 }
+
+
+# Function to re-wire the lattice
+# and generate random graph
+rewire_graph = function(W_latt, p) {
+  n = nrow(W_latt)
+  W_new = W_latt
+  
+  # Get all upper-triangular edge indices
+  edges = which(upper.tri(W_latt), arr.ind = TRUE)
+  weights = W_latt[edges]
+  
+  # Decide which edges to rewire (based on probability p)
+  n_edges = nrow(edges)
+  rewire_mask = runif(n_edges) < p
+  
+  # Indices of edges to rewire and keep
+  rewire_edges = edges[rewire_mask, , drop = FALSE]
+  keep_edges = edges[!rewire_mask, , drop = FALSE]
+  
+  # Clear upper triangle
+  W_new[edges] = 0
+  
+  # Set preserved weights
+  W_new[keep_edges] = weights[!rewire_mask]
+  
+  # Set rewired weights if any
+  if (nrow(rewire_edges) > 0) {
+    shuffled_weights = sample(weights[rewire_mask])
+    W_new[rewire_edges] = shuffled_weights
+  }
+  
+  # We need symmetric matrix
+  W_new[lower.tri(W_new)] = t(W_new)[lower.tri(W_new)]
+  
+  return(W_new)
+}
